68aae450df802365fa704ad7e4423c464b239135
Uttam Yadav <157710495+UttamYadav-DGI@users.noreply.github.com>
2025-07-31 21:45:58 +0530

    Update App.jsx

--- Frontend2/src/App.jsx
+++ Frontend2/src/App.jsx
@@ -3,25 +3,26 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Sidebar from "../components/NavBar";
 import { AuthProvider } from "../components/AuthContext";
-import { ThemeProvider } from '../components/ThemeContext';
-import {ProtectedRoute} from '../components/ProtectedRoute';
+import { ThemeProvider } from "../components/ThemeContext";
+import ProtectedRoute from "../components/ProtectedRoute"; // ✅ Import correctly
+
 const App = () => {
   return (
-     <ProtectedRoute>
     <AuthProvider>
-      <div className="flex flex-col h-screen">
-         <ThemeProvider>
-          <Header/>
-    </ThemeProvider>
-        <div className="flex flex-1">
-          <Sidebar />
-          <main className="flex-1 p-4">
-            <Outlet />
-          </main>
-        </div>
-      </div>
+      <ProtectedRoute>
+        <ThemeProvider>
+          <div className="flex flex-col h-screen">
+            <Header />
+            <div className="flex flex-1">
+              <Sidebar />
+              <main className="flex-1 p-4">
+                <Outlet />
+              </main>
+            </div>
+          </div>
+        </ThemeProvider>
+      </ProtectedRoute>
     </AuthProvider>
-       </ProtectedRoute>
   );
 };
 
