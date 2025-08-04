f3a985d458101fbbd776e4dc3af0ab609760bd3b
Uttam Yadav <157710495+UttamYadav-DGI@users.noreply.github.com>
2025-08-04 21:20:44 +0530

    Update App.jsx

--- Frontend2/src/App.jsx
+++ Frontend2/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import {Outlet, Routes, Route, Navigate } from "react-router-dom";
 import Header from "../components/Header";
 import Sidebar from "../components/NavBar";
 import { AuthProvider } from "../components/AuthContext";
